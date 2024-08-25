19137c38ff9389033e62d903e2931169e7b232e2
richmon2 <retizarichmon84@gmail.com>
2024-08-25 12:07:59 +0800

    chat page added

---
+++ messages.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Messages</title>
+    <link rel="stylesheet" href="./CSS_FILE/message.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <div class="nav">
+        <h1 class="messages">Messages</h1>
+        <a href="#"><p class="dashboard">Dashboard</p></a>
+    </div>
+    <br>
+    <br>
+    <div class="row">
+        <div class=" col-6 aside-left">
+            <div class="header">
+                <img src="./images/chat.png" alt="chatlogo">
+                All Conversation</div>
+            <br>
+            <br>
+            <div class="chathistory">Chat History</div>
+        </div>
+        <div class="col-6 aside-left">
+            <div class="chat-input-container">
+                <div class="input-wrapper">
+                    <input type="text" class="chatmessage" placeholder="Enter a Message">
+                    <button class="send-button"><i class="fas fa-paper-plane"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
